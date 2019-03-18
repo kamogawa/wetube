194dc577cb88e4d86179c9d7b35564147ae6a04a
The9 <captain_kbg@naver.com>
2019-03-18 23:13:50 +0900

    setting controller

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router"
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("hello!");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json({ extended:true }));
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.get("/", handleHome);
-app.use("/user", userRouter);
 
-app.get("/profile", handleProfile);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
